d5634e20cacef67946469b5e2483433d77f3ed7a
2easy4wizzi <giladeini@gmail.com>
2022-12-30 15:39:23 +0200

    Update wu_test.py

--- wu_test.py
+++ wu_test.py
@@ -4,7 +4,8 @@ import wizzi_utils as wu
 def main():
     # TODO 1 change images on md file to this repo
     # TODO 2 md file add repo link
-    # TODO 3 check warning in reqs: update to safe reqs
+    # TODO 3 why req file not appears at user side
+    # TODO 4 check warning in reqs: update to safe reqs
     #           update req txt
     # wu.test_all_modules()
     # wu.test.generate_requirements_file_test(real_req=True)
