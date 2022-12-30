d332bfa4a12cd9460551fe7edafcc5dd83700dab
2easy4wizzi <giladeini@gmail.com>
2022-12-30 10:03:08 +0200

    Create wu_test.py

---
+++ wu_test.py
@@ -0,0 +1,33 @@
+import wizzi_utils as wu
+
+
+def main():
+    # wu.test_all_modules()
+    # wu.test.generate_requirements_file_test(real_req=True)
+    # wu.wizzi_utils_requirements()
+
+    # wu.test.test_all()  # misc package
+    # # wu.got.test.test_all()  # google package
+    # wu.jt.test.test_all()  # json package
+    # wu.cvt.test.test_all()  # open_cv package
+    # wu.pyplt.test.test_all()  # pyplot package
+    # wu.st.test.test_all()  # socket package
+    # wu.tt.test.test_all()  # torch package
+    # wu.tflt.test.test_all()  # tflite package
+    # wu.tts.test.test_all()  # tts package
+    # wu.models.test.test_all()  # models package
+    return
+
+
+if __name__ == '__main__':
+    wu.main_wrapper(
+        main_function=main,
+        seed=42,
+        ipv4=True,
+        cuda_off=False,
+        torch_v=True,
+        tf_v=True,
+        cv2_v=True,
+        with_pip_list=False,
+        with_profiler=False
+    )
